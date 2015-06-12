be096c5b3aabe0fd2fb6a9f427516a5c015531ab
Johnny Wu <KarasAya@gmail.com>
2015-06-12 20:55:16 +0800

    update electron to 0.28.1

--- utils/npm.sh
+++ utils/npm.sh
@@ -1,4 +1,4 @@
 export npm_config_disturl=https://atom.io/download/atom-shell
-export npm_config_target=0.28.0
+export npm_config_target=0.28.1
 export npm_config_arch=x64
 HOME=~/.electron-gyp npm $@
