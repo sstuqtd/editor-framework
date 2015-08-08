096ab2c979c74e6eb64d03059524c73dc383fa2e
Johnny Wu <KarasAya@gmail.com>
2015-08-08 16:59:54 +0800

    make sure branch not found will not broke the task

--- utils/gulp-tasks/setup-tasks.js
+++ utils/gulp-tasks/setup-tasks.js
@@ -14,7 +14,9 @@ gulp.task('setup-branch', function(cb) {
     if ( Fs.existsSync('local-setting.json') ) {
         try {
             var jsonObj = JSON.parse(Fs.readFileSync('local-setting.json'));
-            if (jsonObj.branch) {
+            if (jsonObj.branch &&
+                jsonObj.branch.builtins &&
+                jsonObj.branch.sharedPackages) {
                 return cb();
             } else {
                 hasBranchSetting = false;
@@ -36,14 +38,14 @@ gulp.task('setup-branch', function(cb) {
             obj = JSON.parse(Fs.readFileSync('local-setting.json'));
         }
         obj.branch = {
-            sharedPackages: {},
             builtins: {},
+            sharedPackages: {},
         };
 
-        pjson.builtins.forEach(function(entry) {
+        (pjson.builtins || []).forEach(function(entry) {
             obj.branch.builtins[entry] = "master";
         });
-        pjson.sharedPackages.forEach(function(entry) {
+        (pjson.sharedPackages || []).forEach(function(entry) {
             obj.branch.sharedPackages[entry] = "master";
         });
         Fs.writeFileSync('local-setting.json', JSON.stringify(obj, null, '  '));
