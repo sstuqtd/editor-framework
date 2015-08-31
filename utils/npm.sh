dc27cd0af97ad1c3e8599036f50729ac2ce2f7d2
Johnny Wu <KarasAya@gmail.com>
2015-08-31 11:51:07 +0800

    bump electron to 0.30.6. polymer to 1.1.2

--- utils/npm.sh
+++ utils/npm.sh
@@ -1,4 +1,4 @@
 export npm_config_disturl=https://atom.io/download/atom-shell
-export npm_config_target=0.30.5
+export npm_config_target=0.30.6
 export npm_config_arch=x64
 HOME=~/.electron-gyp npm $@
