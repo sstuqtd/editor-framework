dc46180ddd36cf5d9e143ae3d50403b0994cd3de
Johnny Wu <KarasAya@gmail.com>
2015-09-16 13:50:32 +0800

    update electron to 0.32.3

--- utils/npm.sh
+++ utils/npm.sh
@@ -1,4 +1,4 @@
 export npm_config_disturl=https://atom.io/download/atom-shell
-export npm_config_target=0.32.2
+export npm_config_target=0.32.3
 export npm_config_arch=x64
 HOME=~/.electron-gyp npm $@
