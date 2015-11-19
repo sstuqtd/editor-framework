290aabb29d5b7f098ac2f815e64c9aa75ae91227
Johnny Wu <KarasAya@gmail.com>
2015-11-20 00:49:58 +0800

    add command-group

--- test/main/undo.js
+++ test/main/undo.js
@@ -28,27 +28,113 @@ describe('Editor.Undo', function () {
     }
   });
 
-  describe('Editor.Undo.record', function () {
-    beforeEach(function () {
-      Editor.Undo.clear();
-      Helper.reset();
+  beforeEach(function () {
+    _foo = {};
+    _bar = {};
+
+    Editor.Undo.clear();
+    Helper.reset();
+  });
+
+  it('should record the foo commands', function (done) {
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.a = 'a';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.b = 'b';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.c = 'c';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    expect( Editor.Undo._global._groups.length ).to.be.eql(4);
+    expect( Editor.Undo._global._groups[3]._commands[0].info.json ).to.be.eql(JSON.stringify({
+      a: 'a', b: 'b', c: 'c'
+    }));
+
+    done();
+  });
+
+  it('should undo the foo object correctly', function (done) {
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.a = 'a';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.b = 'b';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.c = 'c';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    Editor.Undo.undo();
+    expect(_foo).to.be.deep.eql({
+      a: 'a',
+      b: 'b',
     });
 
-    it('should record the foo commands', function (done) {
-      _foo.a = 'a';
-      Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
-
-      _foo.b = 'b';
-      Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
-
-      _foo.c = 'c';
-      Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
-
-      expect( Editor.Undo._global.commands.length ).to.be.eql(3);
-
-      done();
+    Editor.Undo.undo();
+    expect(_foo).to.be.deep.eql({
+      a: 'a',
     });
 
+    Editor.Undo.undo();
+    expect(_foo).to.be.deep.eql({});
+
+    done();
+  });
+
+
+  it('should redo the foo object correctly', function (done) {
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.a = 'a';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.b = 'b';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    _foo.c = 'c';
+    Editor.Undo.record('foo', { json: JSON.stringify(_foo) } );
+    Editor.Undo.stash();
+
+    Editor.Undo.undo();
+    Editor.Undo.undo();
+    Editor.Undo.undo();
+    expect(_foo).to.be.deep.eql({});
+
+    Editor.Undo.redo();
+    expect(_foo).to.be.deep.eql({
+      a: 'a',
+    });
+
+    Editor.Undo.redo();
+    expect(_foo).to.be.deep.eql({
+      a: 'a',
+      b: 'b',
+    });
+
+    Editor.Undo.redo();
+    expect(_foo).to.be.deep.eql({
+      a: 'a',
+      b: 'b',
+      c: 'c',
+    });
+
+
+    done();
   });
 
 });
