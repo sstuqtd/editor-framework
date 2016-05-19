3f817495bb94f85380095fe96a73c58bd19f13f7
Johnny Wu <KarasAya@gmail.com>
2016-05-20 01:45:14 +0800

    add color-picker

--- test/renderer/focus-mgr.js
+++ test/renderer/focus-mgr.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Editor.UI.FocusMgr', function () {
-  describe('simple', function () {
+  describe('_getFocusableParent', function () {
     Helper.runElement('editor-framework://test/fixtures/focus-mgr.html', 'nested-shadow-dom', '#wrapper');
     let childEL = null;
 
@@ -33,5 +33,73 @@ describe('Editor.UI.FocusMgr', function () {
       done ();
     });
 
+  });
+
+  describe('_getFocusableFrom', function () {
+    describe('get-focusable-from-01', function () {
+      Helper.runElement('editor-framework://test/fixtures/focus-mgr.html', 'get-focusable-from-01', '#g-0');
+
+      beforeEach(function ( done ) {
+        Editor.Window.center();
+
+        let el = Helper.targetEL.querySelector('[focusable]');
+        if ( el ) {
+          el.focusable = true;
+        }
+
+        done();
+      });
+
+      it('should get g-00100', function ( done ) {
+        let resultEL = Editor.UI.FocusMgr._getFocusableFrom(Helper.targetEL);
+        expect(resultEL.id).to.eql('g-00100');
+
+        done ();
+      });
+    });
+
+    describe('get-focusable-from-02', function () {
+      Helper.runElement('editor-framework://test/fixtures/focus-mgr.html', 'get-focusable-from-02', '#g-0');
+
+      beforeEach(function ( done ) {
+        Editor.Window.center();
+
+        let el = Helper.targetEL.querySelector('[focusable]');
+        if ( el ) {
+          el.focusable = true;
+        }
+
+        done();
+      });
+
+      it('should get g-002', function ( done ) {
+        let resultEL = Editor.UI.FocusMgr._getFocusableFrom(Helper.targetEL);
+        expect(resultEL.id).to.eql('g-002');
+
+        done ();
+      });
+    });
+
+    describe('get-focusable-from-03', function () {
+      Helper.runElement('editor-framework://test/fixtures/focus-mgr.html', 'get-focusable-from-03', '#g-0');
+
+      beforeEach(function ( done ) {
+        Editor.Window.center();
+
+        let el = Helper.targetEL.querySelector('[focusable]');
+        if ( el ) {
+          el.focusable = true;
+        }
+
+        done();
+      });
+
+      it('should get null', function ( done ) {
+        let resultEL = Editor.UI.FocusMgr._getFocusableFrom(Helper.targetEL);
+        expect(resultEL).to.eql(null);
+
+        done ();
+      });
+    });
   });
 });
