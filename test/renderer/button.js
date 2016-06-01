20e573cd156bbe90d5d14ea37c36eb08456354df
Johnny Wu <KarasAya@gmail.com>
2016-06-02 01:07:03 +0800

    add some test case

--- test/renderer/button.js
+++ test/renderer/button.js
@@ -17,16 +17,16 @@ suite(tap, '<ui-button>', {timeout: 2000}, t => {
     done();
   });
 
-  t.test('should have shadow root', t => {
+  t.test('it should have shadow root', t => {
     _newElement(el => {
       t.assert(el.shadowRoot);
       t.end();
     });
   });
 
-  t.test('should focus on element when left mouse down', t => {
+  t.test('it should focus on element when left mouse down on the element', t => {
     _newElement(el => {
-      helper.mousedown( el, 'left' );
+      helper.mousedown(el, 'left');
 
       setTimeout(() => {
         t.equal(el.focused, true);
@@ -35,16 +35,16 @@ suite(tap, '<ui-button>', {timeout: 2000}, t => {
     });
   });
 
-  t.test('should send "click" event when mouse click element', t => {
+  t.test('it should send "click" event when left mouse click on the element', t => {
     _newElement(el => {
       el.addEventListener('click', () => {
         t.end();
       });
-      helper.click( el, 'left' );
+      helper.click(el, 'left');
     });
   });
 
-  t.test('should send "click" event when "space" key down and up on the element', t => {
+  t.test('it should send "click" event when we focus the element and press "space" key', t => {
     _newElement(el => {
       el.addEventListener('click', () => {
         t.end();
@@ -54,7 +54,7 @@ suite(tap, '<ui-button>', {timeout: 2000}, t => {
     });
   });
 
-  t.test('should send "click" event when "enter" key down on the element', t => {
+  t.test('it should send "click" event when we focus the element and press down "enter" key', t => {
     _newElement(el => {
       el.addEventListener('click', () => {
         t.end();
@@ -64,26 +64,63 @@ suite(tap, '<ui-button>', {timeout: 2000}, t => {
     });
   });
 
-  t.test('should not send "click" event when only "space" key up on the element', t => {
+  t.test('it should not send "click" event when we press down "space", then focus the element and press up "space" key', t => {
     _newElement(el => {
       el.addEventListener('click', () => {
         t.assert(false, 'should not recieve click event');
       });
-      helper.focus(el);
-      helper.keyup('space');
+
+      helper.focus(null);
+      helper.keydown('space');
 
       setTimeout(() => {
-        t.end();
-      }, 100);
+        helper.focus(el);
+        helper.keyup('space');
+
+        setTimeout(() => {
+          t.end();
+        }, 100);
+      },10);
     });
   });
 
-  t.test('should send "confirm" event when element clicked', t => {
+  t.test('it should not send "click" event when left mouse down on the element and left mouse up outside the element', t => {
+    _newElement(el => {
+      el.addEventListener('click', () => {
+        t.assert(false, 'should not recieve click event');
+      });
+
+      helper.mousedown(el, 'left');
+      setTimeout(() => {
+        t.assert(el.getAttribute('pressed') !== null);
+        helper.mouseup(document.body, 'left');
+
+        setTimeout(() => {
+          t.assert(el.getAttribute('pressed') === null);
+          t.end();
+        }, 100);
+      }, 10);
+    });
+  });
+
+  t.test('should send "confirm" event when the element clicked', t => {
     _newElement(el => {
       el.addEventListener('confirm', () => {
         t.end();
       });
       helper.click( el, 'left' );
+    });
+  });
+
+  t.test('should send "cancel" event when we press "esc" on a pressed element', t => {
+    _newElement(el => {
+      el.addEventListener('cancel', () => {
+        t.end();
+      });
+
+      helper.focus(el);
+      helper.keydown('space');
+      helper.keydown('esc');
     });
   });
 });
